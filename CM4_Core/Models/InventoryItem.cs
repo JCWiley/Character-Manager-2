f20658cc995716d0f847a3a1ec4ac35948a759ca
JCWiley <jw.git.contact@gmail.com>
2024-12-30 19:40:17 -0800

    Added default values to inventory item.

--- CM4_Core/Models/InventoryItem.cs
+++ CM4_Core/Models/InventoryItem.cs
@@ -3,6 +3,11 @@ namespace CM4_Core.Models
 {
     public class InventoryItem : ModelBaseClass
     {
+        public InventoryItem()
+        {
+            Name = "";
+            Description = "";
+        }
         public string Name { get; set; }
         public string Description { get; set; }
 
