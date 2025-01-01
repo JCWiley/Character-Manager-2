c526214a64de5c7a4827cc8c4e75b3e2bf01bb81
JCWiley <jw.git.contact@gmail.com>
2024-12-31 20:36:00 -0800

    Initial data binding tests

--- CM4_UI/ViewModels/MainViewModel.cs
+++ CM4_UI/ViewModels/MainViewModel.cs
@@ -1,14 +1,16 @@
 ﻿using CM4_Core.LogicalGroupInterfaces;
 using CM4_Core.Models;
+using DynamicData.Binding;
 using System.Collections.Generic;
 
 namespace CM4_UI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    public List<Character> characters { get; set; }
     public MainViewModel(IPeople people)
     {
-        List<Character> characters = people.GetCharacters();
+        characters = people.GetCharacters();
     }
     public string Greeting => "Welcome to Avalonia!";
 }
