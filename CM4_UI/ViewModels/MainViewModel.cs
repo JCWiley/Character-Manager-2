6de74fdc57b37bbb8558b6c61620ff7e7e1c0197
JCWiley <jw.git.contact@gmail.com>
2025-01-08 19:01:14 -0800

    Character initial display and test

--- CM4_UI/ViewModels/MainViewModel.cs
+++ CM4_UI/ViewModels/MainViewModel.cs
@@ -1,16 +1,60 @@
 ﻿using CM4_Core.LogicalGroupInterfaces;
+using CM4_Core.LogicalGroups;
 using CM4_Core.Models;
+using CM4_Core.Service;
 using DynamicData.Binding;
+using ReactiveUI;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace CM4_UI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public List<Character> characters { get; set; }
-    public MainViewModel(IPeople people)
+    IPeople _people;
+    INotifyService _notifyService;
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public MainViewModel(IPeople people,INotifyService notifyService,MenuViewModel menuViewModel)
     {
-        characters = people.GetCharacters();
+        _notifyService = notifyService;
+        _people = people;
+        MenuViewModel = menuViewModel;
+
+        _notifyService.NotifyDataSourceChanged += HandleDataSourceChanged;
     }
-    public string Greeting => "Welcome to Avalonia!";
+
+    private void HandleDataSourceChanged()
+    {
+        Characters = _people.GetCharacters();
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    //---Commands---//
+    public void AddCharacter()
+    {
+        Character character = new Character();
+        character.Name = "tim";
+        _people.AddCharacter(character);
+        Characters = _people.GetCharacters();
+    }
+
+    //---Properties---//
+    MenuViewModel _menuViewModel;
+    public MenuViewModel MenuViewModel
+    {
+        get => _menuViewModel;
+        set => this.RaiseAndSetIfChanged(ref _menuViewModel, value);
+    }
+    //public List<Character> Characters => _people.GetCharacters();
+    List<Character> _characters;
+    public List<Character> Characters
+    {
+        get => _characters;
+        set => this.RaiseAndSetIfChanged(ref _characters, value);
+    }
 }
---
+++ CM4_UI/ViewModels/MenuViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using CM4_Core.LogicalGroupInterfaces;
+using CM4_UI.Menus.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM4_UI.ViewModels
+{
+    public class MenuViewModel(IFilesService filesService,ISettingsManager settingsManager) : ViewModelBase
+    {
+        IFilesService _filesService = filesService;
+        ISettingsManager _settingsManager = settingsManager;
+
+        public async Task NewProjectAsync()
+        {
+            IStorageFile? newFile = await _filesService.GetNewProjectPathFromUser();
+            string? path = newFile?.TryGetLocalPath();
+            Debug.WriteLine("Attempting to open new project with path " + path);
+            if (path != null) 
+            {
+                Debug.WriteLine("Opening new project with path " + path);
+                _settingsManager.NewProject(path);
+            }
+        }
+
+        public async Task OpenProjectAsync()
+        {
+            IStorageFile? newFile = await _filesService.GetExistingProjectPathFromUser();
+            string? path = newFile?.TryGetLocalPath();
+            Debug.WriteLine("Attempting to open existing project with path " + path);
+            if (path != null)
+            {
+                Debug.WriteLine("Opening existing project with path " + path);
+                _settingsManager.OpenProject(path);
+            }
+        }
+    }
+}
