316c26ca2c49fe6419dac67312b25081bbeebf54
JCWiley <jw.git.contact@gmail.com>
2025-01-11 13:33:34 -0800

    Added equality check to inventory item

--- CM4_Core/Models/InventoryItem.cs
+++ CM4_Core/Models/InventoryItem.cs
@@ -1,13 +1,23 @@
 ﻿
 namespace CM4_Core.Models
 {
-    public class InventoryItem : ModelBaseClass
+    public class InventoryItem : ModelBaseClass, IEquatable<InventoryItem>
     {
         public InventoryItem()
         {
             Name = "";
             Description = "";
         }
+
+        public bool Equals(InventoryItem? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return ((ModelBaseClass)this).Equals((ModelBaseClass)other);
+        }
+
         public string Name { get; set; }
         public string Description { get; set; }
 
